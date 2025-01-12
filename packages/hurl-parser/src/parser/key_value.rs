9b55c21d3c0839fd607b952deaf84e1cbae6d82c
Chris <crwebb85@gmail.com>
2025-01-12 17:39:54 -0500

    refactor(parser): cleaned up key value parser

--- packages/hurl-parser/src/parser/key_value.rs
+++ packages/hurl-parser/src/parser/key_value.rs
@@ -1,38 +1,37 @@
-use chumsky::prelude::*;
+use super::types::{InterpolatedString, InterpolatedStringPart, KeyValue};
 use super::{primitives::sp_parser, template::template_parser};
-use super::types::{
-    InterpolatedString, InterpolatedStringPart, KeyValue
-};
-
+use chumsky::prelude::*;
 
 pub fn key_parser() -> impl Parser<char, InterpolatedString, Error = Simple<char>> + Clone {
     //TODO for some reason when I test hurl files with the hurl cli using
     //these escape sequences I get errors. I need to investivate if that is
     //a version issue or if my understanding on this grammar is wrong
-    let key_string_escaped_char = just('\\').ignore_then(
-        just('\\').to('\\')
-            .or(just('#').to('#'))
-            .or(just(':').to(':'))
-            .or(just('b').to('\x08'))
-            .or(just('f').to('\x0C'))
-            .or(just('n').to('\n'))
-            .or(just('r').to('\r'))
-            .or(just('t').to('\t'))
-            .or(just('u').ignore_then(
-                filter(|c: &char| c.is_digit(16))
-                    .repeated()
-                    .exactly(4)
-                    .collect::<String>()
-                    .validate(|digits, span, emit| {
-                        char::from_u32(u32::from_str_radix(&digits, 16).unwrap()).unwrap_or_else(
-                            || {
-                                emit(Simple::custom(span, "invalid unicode character"));
-                                '\u{FFFD}' // unicode replacement character
-                            },
-                        )
-                    }),
-            )),
-    ).labelled("key-string-escaped-char");
+    let key_string_escaped_char = just('\\')
+        .ignore_then(
+            just('\\')
+                .to('\\')
+                .or(just('#').to('#'))
+                .or(just(':').to(':'))
+                .or(just('b').to('\x08'))
+                .or(just('f').to('\x0C'))
+                .or(just('n').to('\n'))
+                .or(just('r').to('\r'))
+                .or(just('t').to('\t'))
+                .or(just('u').ignore_then(
+                    filter(|c: &char| c.is_digit(16))
+                        .repeated()
+                        .exactly(4)
+                        .collect::<String>()
+                        .validate(|digits, span, emit| {
+                            char::from_u32(u32::from_str_radix(&digits, 16).unwrap())
+                                .unwrap_or_else(|| {
+                                    emit(Simple::custom(span, "invalid unicode character"));
+                                    '\u{FFFD}' // unicode replacement character
+                                })
+                        }),
+                )),
+        )
+        .labelled("key-string-escaped-char");
 
     let key_string_text = filter::<_, _, Simple<char>>(|c: &char| {
         c.is_ascii_alphanumeric()
@@ -43,8 +42,8 @@ pub fn key_parser() -> impl Parser<char, InterpolatedString, Error = Simple<char
             || c == &']'
             || c == &'@'
             || c == &'$'
-    }).labelled("key-string-text");
-
+    })
+    .labelled("key-string-text");
 
     let key_string_content = key_string_text
         .or(key_string_escaped_char)
@@ -65,87 +64,89 @@ pub fn key_parser() -> impl Parser<char, InterpolatedString, Error = Simple<char
         .at_least(1)
         .map(|k| InterpolatedString { parts: k })
         .labelled("key-string");
-    
+
     key_string
 }
 
-
 pub fn value_parser() -> impl Parser<char, InterpolatedString, Error = Simple<char>> + Clone {
-
-    let value_string_escaped_char = just('\\').ignore_then(
-        just('\\').to('\\')
-            .or(just('#').to('#'))
-            .or(just('b').to('\x08'))
-            .or(just('f').to('\x0C'))
-            .or(just('n').to('\n'))
-            .or(just('r').to('\r'))
-            .or(just('t').to('\t'))
-            .or(just('u').ignore_then(
-                filter(|c: &char| c.is_digit(16))
-                    .repeated()
-                    .exactly(4)
-                    .collect::<String>()
-                    .validate(|digits, span, emit| {
-                        char::from_u32(u32::from_str_radix(&digits, 16).unwrap()).unwrap_or_else(
-                            || {
-                                emit(Simple::custom(span, "invalid unicode character"));
-                                '\u{FFFD}' // unicode replacement character
-                            },
-                        )
-                    }),
-            )),
-    ).labelled("value-string-escaped-char");
-
-    let value_string_text = filter::<_, _, Simple<char>>(|c: &char| {
-        c != &'#' && c != &'\n' && c != &'\\' 
-            //currly brackets while allowed will be handled after trying to parse
-            //as a template
-            && c != &'{' && c != &'}'
-    }).labelled("value-string-text");
-
-    let value_string_content = value_string_text
-    .or(value_string_escaped_char)
-    .repeated()
-    .at_least(1)
-    .collect::<String>()
-    .map(InterpolatedStringPart::Str).labelled("value-string-content");
-
-    let value_brackets = filter::<_, _, Simple<char>>(|c: &char| {
-        c == &'{' || c == &'}'
-    })
-    .repeated()
-    .at_least(1)
-    .collect::<String>()
-    .map(InterpolatedStringPart::Str).labelled("value_brackets");
+    let value_string_escaped_char = just('\\')
+        .ignore_then(
+            just('\\')
+                .to('\\')
+                .or(just('#').to('#'))
+                .or(just('b').to('\x08'))
+                .or(just('f').to('\x0C'))
+                .or(just('n').to('\n'))
+                .or(just('r').to('\r'))
+                .or(just('t').to('\t'))
+                .or(just('u').ignore_then(
+                    filter(|c: &char| c.is_digit(16))
+                        .repeated()
+                        .exactly(4)
+                        .collect::<String>()
+                        .validate(|digits, span, emit| {
+                            char::from_u32(u32::from_str_radix(&digits, 16).unwrap())
+                                .unwrap_or_else(|| {
+                                    emit(Simple::custom(span, "invalid unicode character"));
+                                    '\u{FFFD}' // unicode replacement character
+                                })
+                        }),
+                )),
+        )
+        .labelled("value-string-escaped-char");
 
     let template = template_parser();
 
-    let value_template_part = template
-        .map(|t| InterpolatedStringPart::Template(t)).labelled("value-template");
+    let value_string_text =
+        filter::<_, _, Simple<char>>(|c: &char| c != &'#' && c != &'\n' && c != &'\\' && c != &'{')
+            .labelled("value-string-text");
 
-    let value_part = value_template_part
-        .or(value_string_content)
-        .or(value_brackets);
-
-    let value_string = value_part
+    let value_string_content = value_string_text
+        .or(value_string_escaped_char)
         .repeated()
         .at_least(1)
-        .map(|v| InterpolatedString { parts: v }).labelled("value-string");
+        .collect::<String>()
+        .map(InterpolatedStringPart::Str)
+        .labelled("value-string-content");
+
+    let value_brackets = filter::<_, _, Simple<char>>(|c: &char| c == &'{')
+        .repeated()
+        .at_least(1)
+        //Consume 2 since if those two brackets made up a template they would have
+        //already been consumed
+        .at_most(2)
+        .collect::<String>()
+        .map(InterpolatedStringPart::Str)
+        .labelled("value_brackets");
+
+    let value_template_part = template
+        .map(|t| InterpolatedStringPart::Template(t))
+        .labelled("value-template");
+
+    //TODO normalize so that non-template brackets get combined with value_string_content
+    let value_string = value_template_part
+        .or(value_string_content)
+        .or(value_brackets)
+        .repeated()
+        .at_least(1)
+        .map(|v| InterpolatedString { parts: v })
+        .labelled("value-string");
 
     value_string
 }
 
 pub fn key_value_parser() -> impl Parser<char, KeyValue, Error = Simple<char>> + Clone {
-
     let sp = sp_parser();
     let key = key_parser();
     let value = value_parser();
 
     let key_value = key
+        .then_ignore(sp.clone().repeated()) //TODO: I think this is an offspec sp
         .then_ignore(just(':'))
-        .then_ignore(sp.repeated())//TODO: I think this is an offspec sp
+        .then_ignore(sp.repeated()) //TODO: I think this is an offspec sp
         .then(value)
-        .map(|(key, value)| KeyValue { key, value }).labelled("key-value");
+        .map(|(key, value)| KeyValue { key, value })
+        .labelled("key-value");
 
     key_value
 }
