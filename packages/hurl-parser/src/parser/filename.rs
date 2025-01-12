2436cb6375d072f48c485cdc02101880d937b944
Chris <crwebb85@gmail.com>
2025-01-12 13:27:20 -0500

    test(parser): filename parser

--- packages/hurl-parser/src/parser/filename.rs
+++ packages/hurl-parser/src/parser/filename.rs
@@ -2,13 +2,26 @@ use crate::parser::template::template_parser;
 use crate::parser::types::{InterpolatedString, InterpolatedStringPart};
 use chumsky::prelude::*;
 
+/// Parses a file path
+///
+/// Note: file paths cannot be files outside the root directory. Where
+/// the root directory is by default files directory the hurl file is in. This can
+/// be changed withe the hurl --file-root option.
+/// TODO: Add LSP Diagnostics for is the file does not exist or if the file is
+/// outside the root directory
+///
+///
+/// # Returns
+/// The filename parser
+///
+/// ```
 pub fn filename_parser() -> impl Parser<char, InterpolatedString, Error = Simple<char>> + Clone {
     let template = template_parser();
 
-    let filename_string_escaped_char = just('\\')
+    let filename_escape_char = just('\\')
         .ignore_then(
             just('\\')
-                .or(just('\\').to('\\'))
+                .to('\\')
                 .or(just('b').to('\x08'))
                 .or(just('f').to('\x0C'))
                 .or(just('n').to('\n'))
@@ -34,25 +47,28 @@ pub fn filename_parser() -> impl Parser<char, InterpolatedString, Error = Simple
                         }),
                 )),
         )
-        .labelled("filename_string_escaped_char");
+        .labelled("filename-escaped-char");
 
-    let filename_str_part = filter::<_, _, Simple<char>>(|c: &char| {
+    let filename_text = filter::<_, _, Simple<char>>(|c: &char| {
         // ~[#;{} \n\\]+
         c != &'#' && c != &';' && c != &'{' && c != &'}' && c != &' ' && c != &'\n' && c != &'\\'
     })
-    .or(filename_string_escaped_char)
-    .repeated()
-    .at_least(1)
-    .collect::<String>()
-    .map(InterpolatedStringPart::Str)
-    .labelled("filename_str");
+    .labelled("filename-text");
+
+    let filename_content = filename_text
+        .or(filename_escape_char)
+        .repeated()
+        .at_least(1)
+        .collect::<String>()
+        .map(InterpolatedStringPart::Str)
+        .labelled("filename-content");
 
     let filename_template_part = template
         .clone()
         .map(|t| InterpolatedStringPart::Template(t))
-        .labelled("filename_template");
+        .labelled("filename-template");
 
-    let filename = filename_str_part
+    let filename = filename_content
         .or(filename_template_part)
         .repeated()
         .at_least(1)
@@ -60,4 +76,280 @@ pub fn filename_parser() -> impl Parser<char, InterpolatedString, Error = Simple
         .labelled("filename");
 
     filename
+}
+
+#[cfg(test)]
+mod expr_tests {
+
+    use super::*;
+    use insta::assert_debug_snapshot;
+
+    #[test]
+    fn it_parses_simple_filename() {
+        let test_str = "example.txt";
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_filename_template() {
+        let test_str = "{{input_file}}";
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Template(
+                        Template {
+                            expr: Expr {
+                                variable: VariableName(
+                                    "input_file",
+                                ),
+                                filters: [],
+                            },
+                        },
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_filename_template_in_file_name() {
+        let test_str = "./tests/{{testFile1}}";
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "./tests/",
+                    ),
+                    Template(
+                        Template {
+                            expr: Expr {
+                                variable: VariableName(
+                                    "testFile1",
+                                ),
+                                filters: [],
+                            },
+                        },
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_relative_directory() {
+        let test_str = r#"./temp/example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "./temp/example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_relative_directory_with_parent_dir_operator() {
+        let test_str = r#"./temp/../example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "./temp/../example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_relative_directory_with_multiple_parent_dir_operator() {
+        let test_str = r#"./temp/../../example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "./temp/../../example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_relative_directory_with_leading_parent_dir_operator() {
+        let test_str = r#"../example/example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "../example/example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_absolute_directory() {
+        let test_str =
+            r#"C:/Users/myuser/Documents/projects/hurl-language-server/examples/example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "C:/Users/myuser/Documents/projects/hurl-language-server/examples/example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_parses_absolute_directory_with_backslashes() {
+        let test_str = r#"C:\\Users\\myuser\\Documents\\projects\\hurl-language-server\\examples\\example.txt"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Ok(
+            InterpolatedString {
+                parts: [
+                    Str(
+                        "C:\\Users\\myuser\\Documents\\projects\\hurl-language-server\\examples\\example.txt",
+                    ),
+                ],
+            },
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_errors_invalid_characters() {
+        let test_str = r#"#"#;
+        assert_debug_snapshot!(
+        filename_parser().parse(test_str),
+            @r#"
+        Err(
+            [
+                Simple {
+                    span: 0..1,
+                    reason: Unexpected,
+                    expected: {
+                        Some(
+                            '\\',
+                        ),
+                        Some(
+                            '{',
+                        ),
+                    },
+                    found: Some(
+                        '#',
+                    ),
+                    label: Some(
+                        "filename",
+                    ),
+                },
+            ],
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_errors_invalid_character_in_filename() {
+        let test_str = r#"test#.txt"#;
+        //using ".then(end())" to parse to end of string to test the error message.
+        //Otherwise, it would successfully parse the valid part of test and ignore
+        //the rest.
+        assert_debug_snapshot!(
+        filename_parser().then(end()).parse(test_str),
+            @r#"
+        Err(
+            [
+                Simple {
+                    span: 4..5,
+                    reason: Unexpected,
+                    expected: {
+                        None,
+                        Some(
+                            '\\',
+                        ),
+                        Some(
+                            '{',
+                        ),
+                    },
+                    found: Some(
+                        '#',
+                    ),
+                    label: Some(
+                        "filename",
+                    ),
+                },
+            ],
+        )
+        "#,
+        );
+    }
+
+    #[test]
+    fn it_errors_invalid_characters_in_filename() {
+        let test_strings = vec![r#"#"#, r#";"#, r#"{"#, r#"}"#, r#" "#, "\n", r#"\"#];
+        for test_str in test_strings {
+            assert!(
+                filename_parser().parse(test_str).is_err(),
+                r#"The filename parser unexpectedly did not error for the string "{}""#,
+                test_str
+            );
+        }
+    }
 }
